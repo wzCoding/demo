20a20acb45d141e5df406aaf21ee408b5cff1382
wzCoding <1429354968@qq.com>
2023-07-29 18:03:23 +0800

    submit the code

--- web-demo/src/App.vue
+++ web-demo/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <div></div>
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
+*{
+  margin:0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- web-demo/src/views/HomeView.vue
+++ web-demo/src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
-     
+    <div class="container">
+      <div class="box">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +24,82 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.home{
+.home {
+  --card-width: 12.5rem;
+  --card-height: 16rem;
+  --card-gap:24rem;
   background: #000;
+  width: 100vw;
+  height: 100vh;
+
+  .container {
+    width: 100%;
+    height: 100%;
+    perspective: 2000px;
+    perspective-origin: top;
+    position: relative;
+    .box {
+      width: var(--card-width);
+      height: var(--card-height);
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-top: calc(var(--card-height) / -2);
+      margin-left: calc(var(--card-width) / -2);
+      transform-style: preserve-3d;
+      animation: cardRotate 16s linear infinite;
+      &:hover{
+        animation-play-state: paused;
+        cursor: pointer;
+      }
+      .card {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: #69c0ff;
+      }
+      .card:nth-child(1){
+        transform: rotateY(36deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(2){
+        transform: rotateY(72deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(3){
+        transform: rotateY(108deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(4){
+        transform: rotateY(144deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(5){
+        transform: rotateY(180deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(6){
+        transform: rotateY(216deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(7){
+        transform: rotateY(252deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(8){
+        transform: rotateY(288deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(9){
+        transform: rotateY(324deg) translateZ(var(--card-gap));
+      }
+      .card:nth-child(10){
+        transform: rotateY(360deg) translateZ(var(--card-gap));
+      }
+
+    }
+    
+    @keyframes cardRotate {
+       from{
+        transform: rotateY(0deg);
+       }
+       to{
+        transform: rotateY(360deg);
+       }
+    }
+
+  }
 }
 </style>
