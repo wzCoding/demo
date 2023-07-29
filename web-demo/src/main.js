f82453afe975492f1df895865af8fa9659995962
wzCoding <1429354968@qq.com>
2023-07-30 01:34:41 +0800

    submit the code

--- web-demo/src/main.js
+++ web-demo/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './mock/mock'
 
 const app = createApp(App)
 
