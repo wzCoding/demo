81a14e4562ef9d0dcf6a1e6cfa11549dee971bc8
wzCoding <1429354968@qq.com>
2023-08-04 23:55:42 +0800

    submit the code

--- demo/src/components/menu-bar/index.js
+++ demo/src/components/menu-bar/index.js
@@ -4,61 +4,61 @@ import { ArrowLeft } from "@element-plus/icons-vue"
 
 export default {
     name: "menuBar",
-    props: {
-        mode: {
-            type: String,
-            default: "vertical",
-            validator: (value) => {
-                return ["vertical", "horizontal"].includes(value)
-            }
-        },
-        collapse: { type: Boolean, default: false },
-        uniqueOpen: { type: Boolean, default: false },
-        defaultActive: { type: String, default: "" },
-        data: {
-            type: Array,
-            default() {
-                return []
-            },
-            require: true
-        }
-    },
-    emits: ['ctrlMenu'],
-    components: {
-        ArrowLeft
-    },
-    setup(props, { emit }) {
+    // props: {
+    //     mode: {
+    //         type: String,
+    //         default: "vertical",
+    //         validator: (value) => {
+    //             return ["vertical", "horizontal"].includes(value)
+    //         }
+    //     },
+    //     collapse: { type: Boolean, default: false },
+    //     uniqueOpen: { type: Boolean, default: false },
+    //     defaultActive: { type: String, default: "" },
+    //     data: {
+    //         type: Array,
+    //         default() {
+    //             return []
+    //         },
+    //         require: true
+    //     }
+    // },
+    // emits: ['ctrlMenu'],
+    // components: {
+    //     ArrowLeft
+    // },
+    // setup(props, { emit }) {
        
-        const { mode, collapse, uniqueOpen, defaultActive } = toRefs(props)
-        const menus = computed(() => {
-            return props.data.map(item => {
-                if (!item.icon.includes('/')) {
-                    item.icon = require(`../../assets/images/${item.icon}`)
-                }
-                return item
-            })
-        })
-        const ctrlArrow = ref("left-arrow")
-        const collapseClass = ref("")
-        const ctrlMenu = () => {
-            emit('ctrlMenu',collapse)
-            ctrlArrow.value = !collapse.value ? "right-arrow" : "left-arrow"
-        }
+    //     const { mode, collapse, uniqueOpen, defaultActive } = toRefs(props)
+    //     const menus = computed(() => {
+    //         return props.data.map(item => {
+    //             if (!item.icon.includes('/')) {
+    //                 item.icon = require(`../../assets/images/${item.icon}`)
+    //             }
+    //             return item
+    //         })
+    //     })
+    //     const ctrlArrow = ref("left-arrow")
+    //     const collapseClass = ref("")
+    //     const ctrlMenu = () => {
+    //         emit('ctrlMenu',collapse)
+    //         ctrlArrow.value = !collapse.value ? "right-arrow" : "left-arrow"
+    //     }
 
-        const router = useRouter()
-        const toPage = (path) => {
-            if (path) router.push(path)
-        }
-        return {
-            mode,
-            collapse,
-            uniqueOpen,
-            defaultActive,
-            menus,
-            ctrlArrow,
-            collapseClass,
-            ctrlMenu,
-            toPage
-        }
-    }
+    //     const router = useRouter()
+    //     const toPage = (path) => {
+    //         if (path) router.push(path)
+    //     }
+    //     return {
+    //         mode,
+    //         collapse,
+    //         uniqueOpen,
+    //         defaultActive,
+    //         menus,
+    //         ctrlArrow,
+    //         collapseClass,
+    //         ctrlMenu,
+    //         toPage
+    //     }
+    // }
 }
