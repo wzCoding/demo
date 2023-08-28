1110722f69c766943dfb117383c1f515867b1fdf
wzCoding <1429354968@qq.com>
2023-08-28 23:43:58 +0800

    submit the code

--- demo/src/store/useHomeStore.js
+++ demo/src/store/useHomeStore.js
@@ -4,7 +4,7 @@ import service from '@/axios'
 const useHomeStore = defineStore("home", () => {
     const menu = ref([])
     const isLoading = ref(true)
-    async function getHomeMenu(val) {
+    async function getHomeMenu() {
         if (!menu.value.length) {
             const params = { id: 'home', type: "menu" }
             const result = await service.post('mock/data', params)
