bc8bdd02c0d1aaf8eb1de2baae80c3a55eb6592f
wzCoding <1429354968@qq.com>
2024-04-04 03:42:19 +0800

    submit the code

--- demo/src/views/charts/index.vue
+++ demo/src/views/charts/index.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="chart-page">
+        <div class="chart-container">
+            <div class="chart-box">
+                <div class="chart-title">全国各地区水果产量统计图（单位：吨）</div>
+                <div class="chart-content" id="chart-1"></div>
+            </div>
+        </div>
         <div class="table-container">
+            <div class="table-title">
+                <span>全国各地区水果产量统计表（单位：吨）</span>
+                <el-button @click="exportData">导出</el-button>
+            </div>
             <el-table header-cell-class-name="header-cell" :data="pageData" border stripe lazy row-key="code"
-                :cell-class-name="handleCellClass" :empty-text="emptyText" :height="438"
+                :cell-class-name="handleCellClass" :empty-text="emptyText" :height="tableHeight"
                 :tree-props="{ children: 'children', hasChildren: 'hasChildren' }" :load="loadData"
                 @cell-click="handleCellClick">
                 <el-table-column v-for="column in tableColumns" :key="column.prop" :prop="column.prop"
@@ -20,10 +30,18 @@
     </div>
 </template>
 <script setup>
-import { ElTable, ElTableColumn, ElPagination, ElConfigProvider } from 'element-plus'
+import { ElButton, ElTable, ElTableColumn, ElPagination, ElMessage, ElConfigProvider } from 'element-plus'
 import { getData } from '@/utils/service'
 import { getRadom } from '@/utils/index'
 import { ref, reactive, computed } from 'vue'
+import { ElMapExportTable } from 'table-excel'
+import * as echarts from 'echarts'
+ElMessage({
+    message: '本页面所有数据均为随机虚构，如有雷同，纯属巧合！',
+    type: 'info',
+    showClose: true,
+    duration:0,
+})
 const locale = {
     name: 'zh-cn',
     el: {
@@ -41,8 +59,9 @@ const locale = {
         }
     }
 }
+let myChart = null
 const dataType = 'map'
-const defaultSize = 10
+const defaultSize = 8
 const layout = ref('total, sizes, prev, pager, next, jumper')
 const currentPage = ref(1)
 const pageSize = ref(defaultSize)
@@ -56,49 +75,162 @@ const pageData = computed(() => {
 })
 const total = computed(() => tableData.value.length)
 const pageSizes = computed(() => [defaultSize, defaultSize * 2, defaultSize * 3, tableData.value.length])
+const tableHeight = computed(() => (Math.min(pageData.value.length, defaultSize) + 1) * 39.8)
 const tableColumns = reactive([
-    {
-        prop: 'province', label: '省份/直辖市', align: 'left', width: 160
-    },
-    {
-        prop: 'apple', label: '苹果', align: 'right'
-    },
-    {
-        prop: 'banana', label: '香蕉', align: 'right'
-    },
-    {
-        prop: 'orange', label: '橙子', align: 'right'
-    },
-    {
-        prop: 'pear', label: '梨', align: 'right'
-    },
-    {
-        prop: 'grape', label: '葡萄', align: 'right'
-    },
-    {
-        prop: 'watermelon', label: '西瓜', align: 'right'
-    },
-    {
-        prop: 'pineapple', label: '菠萝', align: 'right'
-    },
-    {
-        prop: 'strawberry', label: '草莓', align: 'right'
-    }
-
+    { prop: 'name', label: '省份/直辖市', align: 'left' },
+    { prop: 'apple', label: '苹果', align: 'right', chartType: 'bar', color: '#e44812' },
+    { prop: 'banana', label: '香蕉', align: 'right', chartType: 'bar', color: '#e7df36' },
+    { prop: 'orange', label: '橙子', align: 'right', chartType: 'bar', color: '#e7ad36' },
+    { prop: 'grape', label: '葡萄', align: 'right', chartType: 'bar', color: '#916ad7' },
+    { prop: 'passrate', label: '合格率', align: 'right', chartType: 'line', color: '#409eff' }
 ])
+const tooltipFormatter = (params) => {
+    let startElement = `<div class='chart-tooltip'><div class='chart-tooltip-title'>${params[0].axisValueLabel}</div>`
+    const endElement = `</div>`
+    params.forEach(item => {
+        const { color, seriesName, seriesType } = item
+        const data = seriesType === 'line' ? item.data + '%' : item.data.toLocaleString()
+        const content = `<div class='chart-tooltip-item'><div class='chart-tooltip-desc'><div class='${seriesType}-icon' style='--color:${color}'></div><div>${seriesName}</div></div><div class='chart-tooltip-data'>${data}</div></div>`
+        startElement += content
+    })
+    return startElement + endElement
+}
+const chartOptions = {
+    color: ['#409eff'],
+    legend: {
+        show: true,
+        itemWidth: 20,
+        itemHeight: 12,
+        bottom: '10%',
+        data: []
+    },
+    tooltip: {
+        show: true,
+        trigger: 'axis',
+        confine: true,
+        axisPointer: {
+            type: 'shadow'
+        },
+        formatter: tooltipFormatter
+    },
+    dataZoom: [
+        {
+            type: 'slider',
+            show: true,
+            start: 0,
+            end: 10,
+            xAxisIndex: [0],
+            bottom: '5%',
+            height: 16,
+            handleSize: 0,
+            zoomLock: true
+        }
+    ],
+    grid: {
+        top: '10%',
+        left: '1%',
+        right: '1%',
+        bottom: '20%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: [],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            axisLabel: {
+                color: '#333',
+            }
+        },
+        {
+            type: 'value',
+            min: 0,
+            max: 100,
+            interval: 20,
+            splitLine: {
+                lineStyle: {
+                    color: ['#ccc']
+                }
+            },
+            axisLabel: {
+                color: '#333',
+                formatter: function (value) {
+                    return value + '%';
+                }
+            }
+        }
+    ],
+    series: []
+}
+const handleChartData = (data) => {
+    const legend = []
+    const series = []
+    const xAxis = []
+    tableColumns.forEach(column => {
+        if (column.chartType) {
+            legend.push(column.label)
+            series.push({
+                name: column.label,
+                prop: column.prop,
+                type: column.chartType ? column.chartType : 'bar',
+                yAxisIndex: Number(column.chartType === 'line'),
+                barWidth: '20',
+                data: [],
+                itemStyle: {
+                    color: column.color
+                }
+            })
+        }
+    })
+
+    data.forEach(item => {
+        xAxis.push(item.name)
+        series.forEach(serie => {
+            serie.data.push(item[serie.prop])
+        })
+    })
+    return { xAxis, series, legend }
+}
+const initChart = (data) => {
+    if (!myChart) {
+        myChart = echarts.init(document.getElementById('chart-1'))
+    }
+    const { xAxis, series, legend } = handleChartData(data)
+    chartOptions.legend.data = legend
+    chartOptions.xAxis[0].data = xAxis
+    chartOptions.series = series
+    chartOptions.dataZoom[0].show = xAxis.length > 6
+    chartOptions.dataZoom[0].end = (6 / xAxis.length) * 100
+    myChart.setOption(chartOptions)
+}
 const handleTableData = (data) => {
     const result = data.map(item => {
         const { fullname = '', code = '', level = '', filename = '', name = '', childrenNum = false } = item.properties
         const obj = { code, level, filename }
         obj.hasChildren = childrenNum > 0 && code < 150000
-        if(!obj.code) obj.code = name
         for (const column of tableColumns) {
-            obj[column.prop] = column.prop === 'province' ? (fullname || name) : (getRadom(0, 100000) > 90000 ? 0 : getRadom(0, 100000))
+            if (column.prop === 'name') {
+                obj[column.prop] = fullname || name
+            } else if (column.prop !== 'passrate') {
+                obj[column.prop] = getRadom(0, 100000) > 90000 ? 0 : getRadom(0, 100000)
+            } else {
+                obj.passrate = getRadom(0, 100)
+            }
         }
+        if (!obj.code) obj.code = name
         return obj
     })
+    initChart(result)
     return result
 }
+
 const handleSizeChange = (val) => {
     pageSize.value = val
 }
@@ -109,27 +241,66 @@ const handleCellClass = ({ columnIndex, row, column }) => {
     if (columnIndex > 0) {
         return row[column.property] === 0 ? 'cell-zero' : 'cell-not-zero'
     }
-    return 'province-cell'
+    return 'name-cell'
 }
 const formatterCellValue = (row, column, cellValue, index) => {
-    if (column.property !== 'province') {
+    if (column.property !== 'name') {
         cellValue = cellValue.toLocaleString()
+    }
+    if (column.property === 'passrate') {
+        cellValue = cellValue + '%'
     }
     return cellValue
 }
-const handleCellClick = (cell) => {
+const handleCellClick = (row, column, cell) => {
     if (cell.columnIndex === 0) return
+    console.log(cell)
+    console.log(row)
+    console.log(column)
 }
 const loadData = async (row, treeNode, resolve) => {
     const { code } = row
     const res = await getData(code, dataType)
-    console.log(res)
     const mapData = res.objects ? res.objects.default.geometries : res.features
-    resolve(handleTableData(mapData))
+    const tableData = handleTableData(mapData, row)
+    row.children = tableData
+    resolve(tableData)
 }
+const exportData = () => {
+    const sheetTitle = '全国各地区水果产量统计表（单位：吨）' + new Date().toLocaleString()
+    const column = tableColumns.map(col => {
+        return {
+            title: col.label,
+            dataIndex: col.prop
+        }
+    })
+    const getExportData = (data) => {
+        if (!data || !data.length) return []
+        return data.map(item => {
+            const rowObj = {
+                children: getExportData(item.children)
+            }
+            tableColumns.forEach(column => {
+                rowObj[column.prop] = item[column.prop]
+            })
+            return rowObj
+        })
+    }
 
+    const sheetData = getExportData(tableData.value)
+    const instance = new ElMapExportTable({
+        column: column,
+        data: sheetData,
+        treeNode: true,
+        treeField: "name",
+    },
+        {
+            indentSize: 2,
+        })
+    instance.download(sheetTitle)
+
+}
 getData(mapId.value, dataType).then(res => {
-    console.log(res)
     tableData.value = handleTableData(res.objects.default.geometries)
 })
 </script>
@@ -138,13 +309,43 @@ getData(mapId.value, dataType).then(res => {
     padding: 1rem;
     box-sizing: border-box;
 
+    .chart-container {
+        background-color: #fff;
+        width: 100%;
+
+        .chart-title {
+            font-size: 16px;
+            font-weight: bold;
+            color: #333;
+        }
+
+        .chart-content {
+            width: 100%;
+            height: 280px;
+        }
+    }
+
     .table-container {
         width: 100%;
+        background-color: #fff;
+        .table-title {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 0;
+
+            span {
+                font-size: 16px;
+                font-weight: bold;
+                color: #333;
+            }
+        }
 
         .table-pagination {
             display: flex;
             justify-content: flex-end;
-            margin-top: 1rem;
+            margin-top: 0.5rem;
         }
     }
 }
@@ -152,7 +353,7 @@ getData(mapId.value, dataType).then(res => {
 <style lang="scss">
 .el-table {
     color: #333;
-
+    transition: height var(--transition-time) ease;
 }
 
 .el-table--enable-row-hover .el-table__body tr:hover>td.el-table__cell {
@@ -165,15 +366,74 @@ getData(mapId.value, dataType).then(res => {
     font-weight: bold;
 }
 
-.province-cell .cell {
+.name-cell .cell {
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-}
-
-.cell-not-zero {
     color: #409eff;
     text-decoration: underline;
     cursor: pointer;
+}
+
+.chart-tooltip {
+
+    padding: 0 5px;
+    color: #333;
+
+    .chart-tooltip-title {
+        font-weight: 600;
+        padding: 4px 0;
+    }
+
+    .chart-tooltip-item {
+        display: flex;
+        justify-content: space-between;
+        padding: 4px 0;
+        min-width: 120px;
+
+        .chart-tooltip-desc {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+
+            .bar-icon {
+                width: 20px;
+                height: 12px;
+                border-radius: 3px;
+                background-color: var(--color);
+                margin: 1px 4px 0 0;
+            }
+
+            .line-icon {
+                width: 10px;
+                height: 10px;
+                box-sizing: border-box;
+                border-radius: 50%;
+                border: 2px solid var(--color);
+                margin: 1px 8px 0 4px;
+                position: relative;
+
+                &::after,
+                &::before {
+                    content: '';
+                    display: block;
+                    width: 5px;
+                    height: 2px;
+                    background-color: var(--color);
+                    position: absolute;
+                }
+
+                &::after {
+                    top: 2px;
+                    right: -5px;
+                }
+
+                &::before {
+                    top: 2px;
+                    left: -5px;
+                }
+            }
+        }
+    }
 }
 </style>
--- demo/src/views/charts/tools/index.js
+++ demo/src/views/charts/tools/index.js
@@ -1,4 +1,4 @@
-import JSON5 from 'json5'
+import { deepClone } from "@/utils/index"
 async function getMapData(code) {
     if (!code) return
     const url = `https://geojson.cn/api/data/${code}.topo.json`
@@ -13,10 +13,58 @@ async function getMapData(code) {
     const read = await result.body.getReader().read()
     const decoder = new TextDecoder('utf-8')
     const data = decoder.decode(read.value, { stream: true })
-   
-    return JSON5.parse(data)
+    //json数据太大（>=300kb），解析会偶发性报错
+    return JSON.parse(data)
 }
-
+const chartOptions = {
+    color: ['#409eff'],
+    tooltip: {
+        show: true,
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: [],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series: [
+        {
+            name: '消耗量',
+            type: 'bar',
+            barWidth: '20',
+            data: []
+        }
+    ]
+}
+function getChartOptions(data = {}) {
+    const options = deepClone(chartOptions)
+    data.forEach(item => {
+        if(data.color) options.color = data.color
+        if(data.series){
+            
+        }
+    })
+    return options
+}
 export {
-    getMapData
+    getMapData,
+    getChartOptions
 }
