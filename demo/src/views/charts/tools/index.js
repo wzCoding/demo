9b383fe848f8bb2ce2a61a3dcc8bc7b9a046c784
wzCoding <1429354968@qq.com>
2024-04-05 22:42:30 +0800

    submit the code

--- demo/src/views/charts/tools/index.js
+++ demo/src/views/charts/tools/index.js
@@ -1,4 +1,5 @@
-import { deepClone } from "@/utils/index"
+import { debounce } from "@/utils/index"
+import { onMounted, onUnmounted } from "vue"
 async function getMapData(code) {
     if (!code) return
     const url = `https://geojson.cn/api/data/${code}.topo.json`
@@ -16,8 +17,47 @@ async function getMapData(code) {
     //json数据太大（>=300kb），解析会偶发性报错
     return JSON.parse(data)
 }
-function pageScale(el){
 
+//页面缩放
+function pageScale(el) {
+    
+    const scale = () => {
+        //获取元素的宽高
+        const targetWidth = el.clientWidth || 1920
+        const targetHeight = el.clientHeight || 1080
+        //计算元素宽高比
+        const targetRatio = targetWidth / targetHeight || 16 / 9
+
+        //获取当前页面宽高
+        const rootWidth = document.documentElement.clientWidth || document.body.clientWidth
+        const rootHeight = document.documentElement.clientHeight || document.body.clientHeight
+
+        //默认用宽度计算缩放比例
+        const scaleRatio = targetWidth / rootWidth
+
+        //计算页面宽高比
+        const currentRatio = rootWidth / rootHeight
+
+        //设置缩放样式
+        let scaleStyle = `width:${targetWidth}px;height:${targetHeight}px;`
+        if (currentRatio > targetRatio) {
+            //当前页面宽高比大于目标宽高比，说明页面过宽，需要通过缩小高度来适应
+            scaleRatio = targetHeight / rootHeight
+            scaleStyle += `transform: scale(${scaleRatio}) translateX(-50%); left: 50%;`
+        } else {
+            //当前页面宽高比小于目标宽高比，说明页面过高，需要通过缩小宽度来适应
+            scaleRatio = targetWidth / rootWidth
+            scaleStyle += `transform: scale(${scaleRatio});`
+        }
+    }
+    const resize = debounce(scale, 300)
+    onMounted(() => {
+        scale()
+        window.addEventListener('resize', resize)
+    })
+    onUnmounted(() => {
+        window.removeEventListener('resize', resize)
+    })
 }
 export {
     getMapData,
