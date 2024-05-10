074d0bcf8cf99d09f1e28c5544747ae5ea875546
wzCoding <1429354968@qq.com>
2024-05-11 03:41:30 +0800

    submit the code

--- demo/src/views/css/carousel.vue
+++ demo/src/views/css/carousel.vue
@@ -5,54 +5,109 @@
                 <div class="carousel-item">1</div>
                 <div class="carousel-item">2</div>
                 <div class="carousel-item">3</div>
+                <div class="carousel-item">4</div>
+                <div class="carousel-item">5</div>
+            </div>
+            <div class="controls">
+                <div class="backward" @click="moveBackward">{{ `⟨` }}</div>
+                <div class="forward" @click="moveForward">{{ `⟩` }}</div>
+            </div>
+            <div class="indicators">
+                <span v-for="i in defaultLength" :key="i" :class="{ active: i === activeIndex }"
+                    @click="moveTo(i)"></span>
             </div>
         </div>
     </div>
 </template>
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed, onBeforeUnmount } from 'vue'
 import { Timer } from '@/utils/timer'
 
 let timer = new Timer()
+const carouselTimer = ref(null)
+
+// 设置轮播图的一些默认属性
 const loop = ref(true)
+const autoPlay = ref(true)
+
 const carousel = ref(null)
 const carouselBox = ref(null)
-const activeIndex = ref(0)
 const defaultLength = ref(0)
+
+const activeIndex = ref(0)
+const startIndex = ref(0)
+const endIndex = ref(0)
+
 const carouselStyle = computed(() => {
-    const translate = activeIndex.value > 0 ? `${-activeIndex.value}00%` : 0
     return {
-        transform: `translateX(${translate})`,
+        transform: `translateX(${-activeIndex.value}00%)`,
         transition: 'transform 0.5s ease-in-out'
     }
 })
 // 设置无缝轮播 dom 结构
 const setCarousel = () => {
-    const firstNode = carouselBox.value.children[0]
-    const lastNode = carouselBox.value.children[carouselBox.value.children.length - 1]
-    carouselBox.value.insertBefore(lastNode.cloneNode(true), firstNode)
-    carouselBox.value.appendChild(firstNode.cloneNode(true))
-}
+    //设置背景色
+    Array.from(carouselBox.value.children).forEach(item => {
+        item.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+    })
+    if (loop.value) {
+        //设置开始索引
+        activeIndex.value = startIndex.value = 1
+        // 克隆第一张和最后一张
+        const firstNode = carouselBox.value.children[0]
+        const lastNode = carouselBox.value.children[carouselBox.value.children.length - 1]
+        carouselBox.value.insertBefore(lastNode.cloneNode(true), firstNode)
+        carouselBox.value.appendChild(firstNode.cloneNode(true))
+        //设置结束索引
+        endIndex.value = carouselBox.value.children.length - 1
+    }
 
-// 更新轮播图
+    if (autoPlay.value) {
+        updateCarousel()
+    }
+}
 const updateCarousel = () => {
-    moveForward()
+    carouselTimer.value = timer.interval(moveForward, 1000)
+    carousel.value.addEventListener('mouseenter', () => {
+        // 清除定时器
+        timer.clear(carouselTimer.value)
+    })
+    carousel.value.addEventListener('mouseleave', () => {
+        // 重新设置定时器
+        carouselTimer.value = timer.interval(moveForward, 1000)
+    })
 }
 //向前移动
 const moveForward = () => {
-    if(activeIndex.value === defaultLength.value){
-        activeIndex.value = 0
-        return true
+    if (activeIndex.value === defaultLength.value) {
+        activeIndex.value = startIndex.value - 1
+        // 设置过渡效果为 none，以便瞬间跳转到起始位置
+        carouselBox.value.style.transition = `none`
+        carouselBox.value.style.transform = `translateX(${-activeIndex.value}00%)`
+        // 强制渲染
+        carouselBox.value.getBoundingClientRect()
+        // 从起始位置前进
+        moveTo(activeIndex.value + 1)
+    } else {
+        // 从起始位置前进
+        moveTo(activeIndex.value + 1)
     }
-    activeIndex.value++
 }
 //向后移动
 const moveBackward = () => {
-    if(activeIndex.value === -1){
-        activeIndex.value = defaultLength.value - 1
-        return true
+    if (activeIndex.value === startIndex.value) {
+        activeIndex.value = endIndex.value
+        // 设置过渡效果为 none，以便瞬间跳转到结束位置
+        carouselBox.value.style.transition = `none`
+        carouselBox.value.style.transform = `translateX(${-activeIndex.value}00%)`
+        // 强制渲染
+        carouselBox.value.getBoundingClientRect()
+        // 从结束位置后退
+        moveTo(activeIndex.value - 1)
+    } else {
+        // 从结束位置后退
+        moveTo(activeIndex.value - 1)
     }
-    activeIndex.value--
 }
 //移动到指定位置
 const moveTo = (index) => {
@@ -69,9 +124,11 @@ onMounted(() => {
     defaultLength.value = carouselBox.value.children.length
     // 设置轮播图
     setCarousel()
-    //timer.interval(updateCarousel, 1000)
-
-    //carousel.value.addEventListener('mousemove', handleMouseDown)
+})
+onBeforeUnmount(() => {
+    // 清除定时器
+    timer.clear(carouselTimer.value)
+    timer = null
 })
 </script>
 <style lang="scss" scoped>
@@ -79,18 +136,19 @@ onMounted(() => {
     width: 100%;
     height: 100%;
     overflow: hidden;
-    background-color: #333;
+    background-color: #666;
     display: flex;
     justify-content: center;
     align-items: center;
 
     .carousel {
-        width: 200px;
-        height: 100px;
-        //overflow: hidden;
-        border: 4px solid #f00;
+        width: 400px;
+        height: 200px;
+        overflow: hidden;
+        border: 4px solid #000;
         box-sizing: content-box;
-        z-index: 3;
+        position: relative;
+        z-index: 1;
     }
 
     .carousel-box {
@@ -100,26 +158,71 @@ onMounted(() => {
         .carousel-item {
             width: 100%;
             height: 100%;
-            line-height: 100px;
+            line-height: 200px;
             cursor: pointer;
             display: inline-block;
             text-align: center;
             font-size: 48px;
             color: #333;
-
-            &[data-index="0"] {
-                background-color: #409eff;
-            }
-
-            &[data-index="1"] {
-                background-color: wheat;
-            }
-
-            &[data-index="2"] {
-                background-color: rgb(153, 239, 210);
-            }
         }
     }
 
+    .controls {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+
+        div {
+            width: 30px;
+            height: 30px;
+            background-color: rgba(0, 0, 0, 0.5);
+            border-radius: 50%;
+            text-align: center;
+            line-height: 30px;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        .backward {
+            position: absolute;
+            left: 5px;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+
+        .forward {
+            position: absolute;
+            right: 5px;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
+
+    .indicators {
+        position: absolute;
+        bottom: 10px;
+        left: 50%;
+        transform: translateX(-50%);
+        z-index: 2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        span {
+            width: 10px;
+            height: 10px;
+            border-radius: 50%;
+            background-color: rgba(0, 0, 0, 0.5);
+            margin: 0 5px;
+            cursor: pointer;
+
+            &.active {
+                background-color: #ddd;
+            }
+        }
+
+    }
 }
 </style>
