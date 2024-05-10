c035b13daa8caef8eb0ad9e827bfbf9eaf49717e
wzCoding <1429354968@qq.com>
2024-05-10 23:53:36 +0800

    submit the code

--- demo/src/views/css/carousel.vue
+++ demo/src/views/css/carousel.vue
@@ -2,9 +2,9 @@
     <div class="carousel-page">
         <div class="carousel" ref="carousel">
             <div class="carousel-box" ref="carouselBox" :style="carouselStyle">
-                <div class="carousel-item" data-index="0">1</div>
-                <div class="carousel-item" data-index="1">2</div>
-                <div class="carousel-item" data-index="2">3</div>
+                <div class="carousel-item">1</div>
+                <div class="carousel-item">2</div>
+                <div class="carousel-item">3</div>
             </div>
         </div>
     </div>
@@ -14,33 +14,64 @@ import { ref, onMounted, computed } from 'vue'
 import { Timer } from '@/utils/timer'
 
 let timer = new Timer()
+const loop = ref(true)
 const carousel = ref(null)
 const carouselBox = ref(null)
-const carouselStyle = ref(null)
-const activeCarousel = ref(0)
-// 设置轮播图
+const activeIndex = ref(0)
+const defaultLength = ref(0)
+const carouselStyle = computed(() => {
+    const translate = activeIndex.value > 0 ? `${-activeIndex.value}00%` : 0
+    return {
+        transform: `translateX(${translate})`,
+        transition: 'transform 0.5s ease-in-out'
+    }
+})
+// 设置无缝轮播 dom 结构
 const setCarousel = () => {
-    activeCarousel.value = carouselBox.value.children[0].dataset.index
     const firstNode = carouselBox.value.children[0]
     const lastNode = carouselBox.value.children[carouselBox.value.children.length - 1]
     carouselBox.value.insertBefore(lastNode.cloneNode(true), firstNode)
     carouselBox.value.appendChild(firstNode.cloneNode(true))
 }
+
 // 更新轮播图
 const updateCarousel = () => {
-    console.log('updateCarousel:', activeCarousel.value)
-    if (activeCarousel.value >= carouselBox.value.children.length) {
-        activeCarousel.value = 0
+    moveForward()
+}
+//向前移动
+const moveForward = () => {
+    if(activeIndex.value === defaultLength.value){
+        activeIndex.value = 0
+        return true
     }
-    activeCarousel.value++
-    carouselStyle.value = {
-        transition: 'transform 0.5s ease-in-out',
-        transform: `translateX(${-(carousel.value.offsetWidth || 0) * activeCarousel.value}px)`
+    activeIndex.value++
+}
+//向后移动
+const moveBackward = () => {
+    if(activeIndex.value === -1){
+        activeIndex.value = defaultLength.value - 1
+        return true
     }
+    activeIndex.value--
+}
+//移动到指定位置
+const moveTo = (index) => {
+    activeIndex.value = index
+}
+
+const handleMouseUp = (e) => {
+    console.log(e)
+}
+const handleMouseDown = (e) => {
+    console.log(e)
 }
 onMounted(() => {
+    defaultLength.value = carouselBox.value.children.length
+    // 设置轮播图
     setCarousel()
     //timer.interval(updateCarousel, 1000)
+
+    //carousel.value.addEventListener('mousemove', handleMouseDown)
 })
 </script>
 <style lang="scss" scoped>
@@ -54,20 +85,22 @@ onMounted(() => {
     align-items: center;
 
     .carousel {
-        width: 400px;
-        height: 200px;
+        width: 200px;
+        height: 100px;
         //overflow: hidden;
+        border: 4px solid #f00;
+        box-sizing: content-box;
+        z-index: 3;
     }
 
     .carousel-box {
         position: relative;
         white-space: nowrap;
 
-
         .carousel-item {
             width: 100%;
             height: 100%;
-            line-height: 200px;
+            line-height: 100px;
             cursor: pointer;
             display: inline-block;
             text-align: center;
