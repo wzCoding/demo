74aafe15775ddda44865c6fa95e320ce867e4c5f
wzCoding <1429354968@qq.com>
2024-06-14 01:40:07 +0800

    submit the code

--- demo/src/views/canvas/clip.vue
+++ demo/src/views/canvas/clip.vue
@@ -11,9 +11,9 @@
                     <e-svg v-for="tool in editTools" :key="tool" :name="tool" size="24"
                         @click="handleToolClick(tool)"></e-svg>
                 </div>
-                <div class="preview-clip" @mousedown="handleMouseDown" :style="clipRectStyle">
-                    <div v-for="point in clipPoints" :key="point" :class="point" class="clip-area">
-                        <span class="clip-point" :data-point="point"></span>
+                <div class="preview-clip" ref="previewClip" @mousedown="handleMouseDown" :style="clipRectStyle">
+                    <div v-for="point in clipPoints" :key="point" :data-area="point" class="clip-area">
+                        <span :data-point="point" class="clip-point"></span>
                     </div>
                 </div>
             </div>
@@ -41,19 +41,19 @@ const context = computed(() => {
 
 //clip剪裁相关
 const clipReady = ref(false)
-const pointClip = ref(false)
-
-//初始剪裁的矩形的数据（即图片原本矩形的大小）
-let initRect = null
-
-//上一次剪裁的矩形的数据
-let prevRect = null
+const pointMove = ref(false)
+const areaMove = ref(false)
 
 //剪裁开始的点的数据
 let startPoint = { x: 0, y: 0, point: '' }
 
+//初始剪裁的矩形的数据（即图片原本矩形的大小）
+let initRect = { x: 0, y: 0, width: 0, height: 0 }
+//上一次剪裁的矩形的数据
+let prevRect = initRect
 //剪裁完成的矩形的数据
-const clipRect = reactive({ x: 0, y: 0, width: 0, height: 0 })
+const clipRect = reactive(initRect)
+
 //剪裁完成的矩形的样式
 const clipRectStyle = computed(() => {
     return {
@@ -168,58 +168,84 @@ const handleMouseMove = rafThrottle((e) => {
     const { x: pointX, y: pointY, point } = startPoint
     const { x: prevX, y: prevY, width: prevW, height: prevH } = prevRect
 
-    if (pointClip.value) {
+    //计算上下左右所有剪裁点的移动变化量，以左上边界的点为起点 (0, 0) 计算
+    if (pointMove.value) {
+
+        //上边界剪裁点移动变化量与高度变化量计算
         if (point.includes('top')) {
             safeY = pageY < top ? top : (pageY + minHeight >= bottom ? bottom - minHeight : pageY)
             diffY = safeY - pointY
             diffH = -diffY
         }
+
+        //下边界剪裁点移动变化量与高度变化量计算
         if (point.includes('bottom')) {
             safeY = pageY > bottom ? bottom : (pageY - minHeight < top ? top - minHeight : pageY)
             diffH = safeY - pointY
 
         }
+
+        //左边界剪裁点移动变化量与高度变化量计算
         if (point.includes('left')) {
             safeX = pageX < left ? left : (pageX + minWidth >= right ? right - minWidth : pageX)
             diffX = safeX - pointX
             diffW = -diffX
         }
 
+        //右边界剪裁点移动变化量与高度变化量计算
         if (point.includes('right')) {
             safeX = pageX > right ? right : (pageX - minWidth < left ? left - minWidth : pageX)
             diffW = safeX - pointX
         }
+
+        //更新剪裁框的坐标与宽高
+        clipRect.y = prevY + diffY < 0 ? 0 : prevY + diffY
+        clipRect.height = prevH + diffH > height ? height : prevH + diffH
+
+        clipRect.x = prevX + diffX < 0 ? 0 : prevX + diffX
+        clipRect.width = prevW + diffW > width ? width : prevW + diffW
+
+        //最小宽度限制
+        if (clipRect.width < minWidth) {
+            clipRect.width = minWidth
+            pointMove.value = false
+        }
+        if (clipRect.height < minHeight) {
+            clipRect.height = minHeight
+            pointMove.value = false
+        }
+
     }
-
-    clipRect.y = prevY + diffY < 0 ? 0 : prevY + diffY
-    clipRect.height = prevH + diffH > height ? height : prevH + diffH
-
-    clipRect.x = prevX + diffX < 0 ? 0 : prevX + diffX
-    clipRect.width = prevW + diffW > width ? width : prevW + diffW
-
-    if (clipRect.width < minWidth) {
-        clipRect.x = prevX
-        clipRect.width = minWidth
-    }
-    if (clipRect.height < minHeight) {
-        clipRect.y = prevY
-        clipRect.height = minHeight
+    //计算剪裁区域的移动变化量
+    else if (areaMove.value) {
+        //计算最大可移动的距离
+        const maxDiffX = width - prevW
+        const maxDiffY = height - prevH
+        //计算移动的变化量
+        diffX = prevX + (pageX - pointX)
+        diffY = prevY + (pageY - pointY)
+        //更新剪裁框的坐标完成移动
+        clipRect.x = diffX < 0 ? 0 : (diffX >= maxDiffX ? maxDiffX : diffX)
+        clipRect.y = diffY < 0 ? 0 : (diffY >= maxDiffY ? maxDiffY : diffY)
     }
 })
 
 
 const handleMouseDown = (e) => {
-    const point = e.target.dataset.point
+    const { point, area } = e.target.dataset
     if (point && clipPoints.includes(point)) {
-        pointClip.value = true
+        areaMove.value = false
+        pointMove.value = true
         startPoint.point = point
-        startPoint.x = e.x
-        startPoint.y = e.y
-        prevRect = deepClone(clipRect)
-
-        console.log('mouse down')
-
     }
+    else if (!point && area) {
+        pointMove.value = false
+        areaMove.value = true
+        startPoint.point = area
+    }
+    startPoint.x = e.pageX
+    startPoint.y = e.pageY
+    prevRect = deepClone(clipRect)
 
     document.addEventListener('mousemove', handleMouseMove)
     document.addEventListener('mouseup', handleMouseUp)
@@ -227,8 +253,8 @@ const handleMouseDown = (e) => {
 
 const handleMouseUp = (e) => {
     console.log('mouse up')
-    pointClip.value = false
-
+    pointMove.value = false
+    areaMove.value = false
     document.removeEventListener('mousemove', handleMouseMove)
     document.removeEventListener('mouseup', handleMouseUp)
 }
@@ -239,6 +265,7 @@ const clearRect = () => {
     clipRect.width = 0
     clipRect.height = 0
 }
+
 onMounted(() => {
     const { width, height } = getComputedStyle(previewImg.value)
     canvas.value = new myCanvas({
@@ -251,6 +278,11 @@ onMounted(() => {
         }
     })
 })
+onUnmounted(() => {
+    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mouseup', handleMouseUp)
+})
+
 </script>
 <style lang="scss" scoped>
 .canvas-box {
@@ -344,7 +376,7 @@ onMounted(() => {
                 background-color: var(--theme-gradient-color-1);
             }
 
-            &.top-left {
+            &[data-area='top-left'] {
                 grid-area: top-left;
                 border-left: unset;
                 border-top: unset;
@@ -360,7 +392,7 @@ onMounted(() => {
                 }
             }
 
-            &.top {
+            &[data-area='top'] {
                 grid-area: top;
                 border-top: unset;
                 border-left: unset;
@@ -378,7 +410,7 @@ onMounted(() => {
                 }
             }
 
-            &.top-right {
+            &[data-area='top-right'] {
                 grid-area: top-right;
                 border-right: unset;
                 border-top: unset;
@@ -395,7 +427,7 @@ onMounted(() => {
                 }
             }
 
-            &.left {
+            &[data-area='left'] {
                 grid-area: left;
                 border-left: unset;
                 border-top: unset;
@@ -413,12 +445,12 @@ onMounted(() => {
                 }
             }
 
-            &.center {
+            &[data-area='center'] {
                 grid-area: center;
                 border: unset;
             }
 
-            &.right {
+            &[data-area='right'] {
                 grid-area: right;
                 border-right: unset;
                 border-top: unset;
@@ -437,7 +469,7 @@ onMounted(() => {
                 }
             }
 
-            &.bottom-left {
+            &[data-area='bottom-left'] {
                 grid-area: bottom-left;
                 border-left: unset;
                 border-bottom: unset;
@@ -454,7 +486,7 @@ onMounted(() => {
                 }
             }
 
-            &.bottom {
+            &[data-area='bottom'] {
                 grid-area: bottom;
                 border-bottom: unset;
                 border-left: unset;
@@ -473,7 +505,7 @@ onMounted(() => {
                 }
             }
 
-            &.bottom-right {
+            &[data-area='bottom-right'] {
                 grid-area: bottom-right;
                 border-right: unset;
                 border-bottom: unset;
