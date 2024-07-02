872c5541a56aafebb98c429e22c3af56f1785113
wzCoding <1429354968@qq.com>
2024-07-02 11:03:11 +0800

    submit the code

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { Message } from '@/components/Message'
 import Home from '../views/home/home.vue'
 const routes = [
   {
@@ -32,8 +33,24 @@ const router = createRouter({
 router.beforeEach(to => {
   const basePath = to.fullPath.split("/").filter(Boolean)[0]
   const route = routes.filter(item => item.name === basePath)[0]
+  
   if (to.name === 'toHome') {
     return route ? route.path : "/"
-  } 
+  }
+
+  if (to.name === 'charts') {
+    const isMobile = 'ontouchstart' in document.documentElement
+    //判断是否移动端
+    if (isMobile) {
+      Message.info({
+        type:"info",
+        text: "当前页面不支持移动端浏览",
+        showClose: true
+      })
+
+      return "/"
+    }
+  }
 })
+
 export default router
